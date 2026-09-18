b5d113c0c484004cf82a31301c729c9d393f087c
Porter Rasmussen <porter.rasmussen@students.snow.edu>
2026-09-18 09:38:58 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("                                       ");
+Console.SetCursorPosition(3,2);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("   ");
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.SetCursorPosition(10,3);
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("   ");
