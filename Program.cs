3090191481e43e145855de865b4297cff972d8b4
Porter Rasmussen <porter.rasmussen@students.snow.edu>
2026-09-18 09:58:06 -0600

    second commit. Stuff is starting to work well

--- Program.cs
+++ Program.cs
@@ -1,14 +1,71 @@
 ﻿Console.Clear();
 
+//Row one
 Console.BackgroundColor = ConsoleColor.DarkBlue;
-Console.Write("                                       ");
-Console.SetCursorPosition(3,2);
+Console.Write("                            ");
+
+//Row two
+Console.SetCursorPosition(0,1);
+Console.Write("        ");
 Console.BackgroundColor = ConsoleColor.Red;
-Console.Write("   ");
+Console.Write("          ");
 Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("          ");
 
+//Row three
+Console.SetCursorPosition(0,2);
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("                  ");
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("    ");
 
+//Row four
+Console.SetCursorPosition(0,3);
+Console.Write("      ");
 Console.BackgroundColor = ConsoleColor.Black;
-Console.SetCursorPosition(10,3);
-Console.BackgroundColor = ConsoleColor.Red;
-Console.Write("   ");
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("        ");
+
+//Row five
+Console.SetCursorPosition(0,4);
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("    ");
+
+//Row six
+Console.SetCursorPosition(0,5);
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("    ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("  ");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("      ");
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("  ");
--- obj/Debug/net10.0/lab_3.AssemblyInfo.cs
+++ obj/Debug/net10.0/lab_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d113c0c484004cf82a31301c729c9d393f087c")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
